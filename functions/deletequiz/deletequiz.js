bd29c1357efb91a86ebebdd1bf37add8d0ad9a8d
MapYh <145336421+MapYh@users.noreply.github.com>
2024-09-30 20:41:41 +0200

    Added starting code for a way to delete a quiz.

--- functions/deletequiz/deletequiz.js
+++ functions/deletequiz/deletequiz.js
@@ -14,7 +14,6 @@ const handler = middy()
       const quizTable = process.env.QUIZ_TABLE;
       console.log("quizTable", quizTable);
 
-      const question = JSON.parse(event.body);
       const { name } = JSON.parse(event.body);
       console.log("name", name);
       console.log("question", question);
@@ -25,53 +24,14 @@ const handler = middy()
           name,
         },
       };
-
       console.log("getParams", getParams);
       const result = await db.send(new GetCommand(getParams));
       console.log("result", result);
+
       if (!result.Item) {
         return sendResponse({
           message: "Could not find a quiz with that name",
         });
-      } else {
-        console.log("result", result.Item);
-
-        console.log("result", result.Item.userid);
-        console.log("event.id", event.id);
-        if (event.id != result.Item.userid) {
-          return sendResponse({ message: "Wrong account for that quiz." });
-        }
-        const newquestion = {
-          TableName: quizTable,
-          Item: {
-            name: name,
-            quizId: question.quizId,
-            questions: [question],
-            userid: event?.id,
-          },
-        };
-        const getParamstwo = {
-          TableName: quizTable,
-          Key: {
-            name,
-          },
-        };
-
-        const putResult = await db.send(new PutCommand(newquestion));
-        const getResult = await db.send(new GetCommand(getParamstwo));
-        console.log("putResult", putResult);
-        console.log("getResult", getResult.Item);
-        if (putResult) {
-          return sendResponse({
-            success: true,
-            message: getResult.Item,
-          });
-        } else {
-          return sendResponse({
-            success: false,
-            message: "No new question added.",
-          });
-        }
       }
     } catch (error) {
       return sendError(500, { success: false, message: error });
