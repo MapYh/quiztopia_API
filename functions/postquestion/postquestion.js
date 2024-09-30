629ea21194c5f27dcf270d667cca0b0b07b29383
MapYh <145336421+MapYh@users.noreply.github.com>
2024-09-30 20:22:13 +0200

    Added a way to post questions to quizes.

---
+++ functions/postquestion/postquestion.js
@@ -0,0 +1,86 @@
+import { sendResponse, sendError } from "../../utils/sendResponse.js";
+const { PutCommand, GetCommand } = require("@aws-sdk/lib-dynamodb");
+const db = require("../../services/db.js");
+import middy from "@middy/core";
+
+const { validateToken } = require("../../services/auth");
+
+const handler = middy()
+  .handler(async (event) => {
+    try {
+      if (!event?.id || (event?.error && event?.error === "401"))
+        return sendError(401, { success: false, message: "Invalid token" });
+
+      const quizTable = process.env.QUIZ_TABLE;
+      console.log("quizTable", quizTable);
+
+      const question = JSON.parse(event.body);
+      const { name } = JSON.parse(event.body);
+      console.log("name", name);
+      console.log("question", question);
+
+      const getParams = {
+        TableName: quizTable,
+        Key: {
+          name,
+        },
+      };
+
+      console.log("getParams", getParams);
+      const result = await db.send(new GetCommand(getParams));
+      console.log("result", result);
+      if (!result.Item) {
+        return sendResponse({
+          message: "Could not find a quiz with that name",
+        });
+      } else {
+        console.log("result", result.Item);
+
+        console.log("result", result.Item.userid);
+        console.log("event.id", event.id);
+        if (event.id != result.Item.userid) {
+          return sendResponse({ message: "Wrong account for that quiz." });
+        }
+        const newquestion = {
+          TableName: quizTable,
+          Item: {
+            name: name,
+            quizId: question.quizId,
+            questions: [question],
+            userid: event?.id,
+          },
+        };
+        const getParamstwo = {
+          TableName: quizTable,
+          Key: {
+            name,
+          },
+        };
+        /*   const newQuiz = {
+             TableName: quizTable,
+             Item: { name: name, quizId: id, questions: [], userid: event?.id },
+           }; */
+
+        const putResult = await db.send(new PutCommand(newquestion));
+        const getResult = await db.send(new GetCommand(getParamstwo));
+        console.log("putResult", putResult);
+        console.log("getResult", getResult.Item);
+        if (putResult) {
+          return sendResponse({
+            success: true,
+            message: getResult.Item,
+          });
+        } else {
+          return sendResponse({
+            success: false,
+            message: "No new question added.",
+          });
+        }
+      }
+    } catch (error) {
+      return sendError(500, { success: false, message: error });
+    }
+  })
+  .use(validateToken);
+
+module.exports = { handler };
--- functions/postquiz/postquiz.js
+++ functions/postquiz/postquiz.js
@@ -31,29 +31,33 @@ const handler = middy()
       console.log("newQuiz", newQuiz);
       console.log("getParams", getParams);
       const result = await db.send(new GetCommand(getParams));
-      console.log("result", result.Item);
+      console.log("result outside ", result);
+      if (!result.Item) {
+        const putResult = await db.send(new PutCommand(newQuiz));
+        console.log("putResult", putResult);
+        if (putResult) {
+          return sendResponse({
+            success: true,
+            message: `A new quiz added. With id: ${id}`,
+          });
+        } else {
+          return sendResponse({
+            success: false,
+            message: "No new quiz added.",
+          });
+        }
+      } else {
+        console.log("result", result.Item);
 
-      console.log("result", result.Item.userid);
-      console.log("event.id", event.id);
-      if (event.id != result.Item.userid) {
-        return sendResponse({ message: "Wrong account for that quiz." });
-      }
-
-      if (result.Item) {
+        console.log("result", result.Item.userid);
+        console.log("event.id", event.id);
+        if (event.id != result.Item.userid) {
+          return sendResponse({ message: "Wrong account for that quiz." });
+        }
         return sendError(400, {
           success: false,
           message: "A quiz with that name already exists.",
         });
-      }
-      const putResult = await db.send(new PutCommand(newQuiz));
-      console.log("putResult", putResult);
-      if (putResult) {
-        return sendResponse({
-          success: true,
-          message: `A new quiz added. With id: ${id}`,
-        });
-      } else {
-        return sendResponse({ success: false, message: "No new quiz added." });
       }
     } catch (error) {
       return sendError(500, { success: false, message: error });
