6c95e4a82625f84dd3c9ea47b7c4e8971d38b09b
MapYh <145336421+MapYh@users.noreply.github.com>
2024-10-03 22:37:41 +0200

    Added a endpoint to get all the highscores for the quizes.

---
+++ functions/getleaderboard/getleaderboard.js
@@ -0,0 +1,36 @@
+import { sendResponse, sendError } from "../../utils/sendResponse.js";
+const { ScanCommand } = require("@aws-sdk/client-dynamodb");
+const db = require("../../services/db.js");
+import middy from "@middy/core";
+const {getvalidation} = require("../../services/requestValidation/getvalidation.js");
+
+
+const handler = middy()
+  .handler(async (event) => {
+    try {
+      
+      if (event.error == "400")
+      return sendError(400, { success: false, message: "Invalid event body, it should be empty." });
+
+
+      const leaderTable = process.env.LEADER_TABLE;
+      const params = {
+        TableName: leaderTable,
+      }
+      const result = await db.send(new ScanCommand(params));
+     console.log("result", result);
+      let leaderArray = []
+      result.Items.forEach((object)=>{
+      
+        leaderArray.push({"Quizname": object.name,"Userid": object.userId, "Highscore": object.points})
+        
+      })
+    
+      return sendResponse({success: true, message: leaderArray});
+    } catch (error) {
+      return sendError(500, { success: false, message: error.message });
+    }
+  }).use(getvalidation);
+
+
+module.exports = { handler };
